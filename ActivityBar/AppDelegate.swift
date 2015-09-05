e65cd4a989256cc37e801184024600ac5ebb73db
Nils Breunese <nils@breun.nl>
2015-09-05 13:16:38 +0200

    Initial Commit

---
+++ ActivityBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ActivityBar
+//
+//  Created by Nils Breunese on 05-09-15.
+//  Copyright © 2015 breun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
