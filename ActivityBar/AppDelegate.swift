ba0304481ed2ba2cba62b93cdae1fe0fb0bd0f68
Nils Breunese <nils@breun.nl>
2015-09-05 17:22:23 +0200

    Added SystemKit as a git submodule.

--- ActivityBar/AppDelegate.swift
+++ ActivityBar/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import SystemKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -16,31 +17,58 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        if let icon = NSImage(named: "statusIcon") {
-            icon.template = true
-            statusItem.image = icon
-            statusItem.menu = statusMenu
-        }
+        let icon = NSImage(named: "statusIcon")
+        icon?.template = true
+        statusItem.button?.image = icon
+        
+        statusItem.menu = statusMenu
+        
+        start()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
     
+    func start() {
+        // TODO: Don't block main UI thread, use a timer
+        // http://stackoverflow.com/questions/29561476/run-background-task-as-loop-in-swift
+        var system = System()
+        while true {
+            let usageCPU = system.usageCPU()
+            print("System: \(usageCPU.system)%")
+            print("User: \(usageCPU.user)%")
+            print("Idle: \(usageCPU.idle)%")
+            print("Nice: \(usageCPU.nice)%")
+            
+            print("----")
+            
+            let memoryUsage = System.memoryUsage()
+            print("Free: \(memoryUsage.free) GB")
+            print("Active: \(memoryUsage.active) GB")
+            print("Inactive: \(memoryUsage.inactive) GB")
+            print("Wired: \(memoryUsage.wired) GB")
+            print("Compressed: \(memoryUsage.compressed) GB")
+            
+            print("----")
+            
+            sleep(2)
+        }
+    }
+    
     @IBAction func openActivityMonitor(sender: NSMenuItem) {
-        let task = NSTask()
-        task.launchPath = "/Applications/Utilities/Activity Monitor.app/Contents/MacOS/Activity Monitor"
-        task.launch()
+        launch("/Applications/Utilities/Activity Monitor.app")
     }
     
     @IBAction func openConsole(sender: NSMenuItem) {
-        let task = NSTask()
-        task.launchPath = "/Applications/Utilities/Console.app/Contents/MacOS/Console"
-        task.launch()
+        launch("/Applications/Utilities/Console.app")
     }
     
     @IBAction func quit(sender: NSMenuItem) {
         exit(0)
     }
+    
+    private func launch(appName: String) {
+        NSWorkspace.sharedWorkspace().launchApplication(appName)
+    }
 }
-
