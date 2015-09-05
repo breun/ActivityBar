94a27bbc54f9dc2dc62bce54b52d9a27bad5a31f
Nils Breunese <nils@breun.nl>
2015-09-05 14:11:11 +0200

    Implemented menu actions.

--- ActivityBar/AppDelegate.swift
+++ ActivityBar/AppDelegate.swift
@@ -30,12 +30,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func openActivityMonitor(sender: NSMenuItem) {
+        let task = NSTask()
+        task.launchPath = "/Applications/Utilities/Activity Monitor.app/Contents/MacOS/Activity Monitor"
+        task.launch()
     }
     
     @IBAction func openConsole(sender: NSMenuItem) {
+        let task = NSTask()
+        task.launchPath = "/Applications/Utilities/Console.app/Contents/MacOS/Console"
+        task.launch()
     }
     
     @IBAction func quit(sender: NSMenuItem) {
+        exit(0)
     }
 }
 
